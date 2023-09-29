e3f96b220b2ae36dcd2a6ab7d1ccc9bf1ed199b7
hittanshu <hittupadhyay@gmail.com>
2023-09-29 17:23:53 +0530

    user db
    users can register and login

--- app/src/main/java/com/example/cartor/MainActivity.java
+++ app/src/main/java/com/example/cartor/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cartor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.WindowCompat;
 
@@ -7,12 +8,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText loginusername, loginpass;
+    Button loginbutton;
     TextView register;
 
     @Override
@@ -20,13 +32,86 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS); //make navbar transparent
         setContentView(R.layout.activity_main);
+
         register = findViewById(R.id.register);
-        register.setOnClickListener(new View.OnClickListener() {
+        loginusername = findViewById(R.id.loginusername);
+        loginpass = findViewById(R.id.loginpass);
+        loginbutton = findViewById(R.id.loginbutton);
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, RegisterPage.class);
-                startActivity(i);
+                if(!validateusername() | !validatepassword()){
+
+                }else{
+                    checkUser();
+                }
             }
         });
+    }
+
+    public Boolean validateusername(){
+        String user = loginusername.getText().toString();
+        if(user.isEmpty()){
+            loginusername.setError("Username cannot be empty!");
+            return false;
+        }else{
+            loginusername.setError(null);
+            return true;
+        }
+    }
+
+    public Boolean validatepassword(){
+        String pass = loginpass.getText().toString();
+        if(pass.isEmpty()){
+            loginpass.setError("Password cannot be empty!");
+            return false;
+        }else{
+            loginpass.setError(null);
+            return true;
+        }
+    }
+
+    public void checkUser(){
+        String username = loginusername.getText().toString().trim();
+        String password = loginpass.getText().toString().trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+        Query checkUserDatabase = reference.orderByChild("username").equalTo(username);
+
+        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                if (snapshot.exists()){
+                    loginusername.setError(null);
+                    String passwordfromDB = snapshot.child(username).child("password").getValue(String.class);
+
+                    if(passwordfromDB.equals(password)){
+                        loginusername.setError(null);
+                        Intent i = new Intent(MainActivity.this, HomePage.class);
+                        startActivity(i);
+                    }else{
+                        loginpass.setError("Invalid Credentials!");
+                        loginpass.requestFocus();
+                    }
+                }else{
+                    loginusername.setError("User does not exist!");
+                    loginusername.requestFocus();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
+
+    public void registertv(View view){
+        Intent i = new Intent(MainActivity.this, RegisterPage.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/cartor/RegisterPage.java
+++ app/src/main/java/com/example/cartor/RegisterPage.java
@@ -2,19 +2,57 @@ package com.example.cartor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterPage extends AppCompatActivity {
+
+    EditText registerfull, registeruser, registeremail, registerpass;
+    Button registerbutton;
+    FirebaseDatabase database;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activity_register_page);
+
+        registerfull = findViewById(R.id.registerfull);
+        registeruser = findViewById(R.id.registeruser);
+        registeremail = findViewById(R.id.registeremail);
+        registerpass = findViewById(R.id.registerpass);
+        registerbutton = findViewById(R.id.registerbutton);
+
+        registerbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                database = FirebaseDatabase.getInstance();
+                reference = database.getReference("users");
+
+                String name = registerfull.getText().toString();
+                String user = registeruser.getText().toString();
+                String email = registeremail.getText().toString();
+                String pass = registerpass.getText().toString();
+
+                HelperClass helperClass = new HelperClass(name, user, email, pass);
+                reference.child(user).setValue(helperClass);
+
+                Toast.makeText(RegisterPage.this, "You have signed up successfully!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(RegisterPage.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 
