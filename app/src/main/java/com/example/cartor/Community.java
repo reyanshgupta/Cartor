5007d18a5e09474413213f6cb34f01e0706f995a
Reyansh Gupta <happygupta@aol.in>
2023-10-17 16:29:22 +0530

    feat: menubar and community page

---
+++ app/src/main/java/com/example/cartor/Community.java
@@ -0,0 +1,12 @@
+package com.example.cartor;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class Community extends AppCompatActivity {
+
+}
