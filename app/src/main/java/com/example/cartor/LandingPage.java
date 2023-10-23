0cc276c1657cf7d8676376bb44b17f001fd9f2df
Reyansh Gupta <happygupta@aol.in>
2023-10-23 19:51:58 +0530

    751

--- app/src/main/java/com/example/cartor/LandingPage.java
+++ app/src/main/java/com/example/cartor/LandingPage.java
@@ -1,10 +1,20 @@
 package com.example.cartor;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class LandingPage extends AppCompatActivity {
 
@@ -14,7 +24,17 @@ public class LandingPage extends AppCompatActivity {
         setContentView(R.layout.activity_landing_page);
 
         BottomNavigationView bottomNavigation = findViewById(R.id.bottom_navigation);
+        Button py = findViewById(R.id.py);
+        py.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText outputEditText = findViewById(R.id.et);
+                String pythonOutput = runPythonScript();
 
+                // Display the Python output in the EditText
+                outputEditText.setText(pythonOutput);
+            }
+        });
         bottomNavigation.setOnNavigationItemSelectedListener(item -> {
             int itemId = item.getItemId();
             if (itemId == R.id.navigation_home) {
@@ -41,6 +61,35 @@ public class LandingPage extends AppCompatActivity {
         }
     }
 
+    private String runPythonScript() {
+        // Execute your Python script and capture its output
+        String pythonOutput = ""; // This will store the Python script's output
+
+        try {
+            String pythonScript = "test.py";
+            ProcessBuilder processBuilder = new ProcessBuilder("python", pythonScript);
+            processBuilder.directory(new File("test.py"));
+            Process process = processBuilder.start();
+
+            // Read the output
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                pythonOutput += line + "\n"; // Append each line to the output
+            }
+
+            // Wait for the process to finish
+            int exitCode = process.waitFor();
+
+            // Print the exit code (0 usually means success)
+            System.out.println("Python script exited with code " + exitCode);
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return pythonOutput;
+    }
     protected void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
