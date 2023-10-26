11b97996431518b6c520c8222b734ef149b2a47c
Reyansh Gupta <happygupta@aol.in>
2023-10-26 13:08:02 +0530

    final

--- app/src/main/java/com/example/cartor/MarketplaceFragment.java
+++ app/src/main/java/com/example/cartor/MarketplaceFragment.java
@@ -1,5 +1,6 @@
 package com.example.cartor;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +28,10 @@ public class MarketplaceFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    String[] couponNames = {"Plane Tickets", "Travel", "Food Coupons", "Movie Tickets"};
+    int[] Images = {R.drawable.ic_plane, R.drawable.ic_car, R.drawable.ic_food, R.drawable.ic_movie};
+    String[] cartorCreditsCost = {"200", "300", "500", "100"};
+    private SimpleAdapter sa;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -59,6 +72,23 @@ public class MarketplaceFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_marketplace, container, false);
+        View view = inflater.inflate(R.layout.fragment_marketplace, container, false);
+
+        ListView listView = view.findViewById(R.id.listview);  // Check for null here
+        if (listView != null) {
+            List<HashMap<String, String>> itemList = new ArrayList<>();
+            for (int i = 0; i < 4; i++) {
+                HashMap<String, String> itemMap = new HashMap<>();
+                itemMap.put("Name", couponNames[i]);
+                itemMap.put("Images", String.valueOf(Images[i]));
+                itemList.add(itemMap);
+            }
+
+            String[] from = {"Name", "Images"};
+            int[] to = {R.id.tv, R.id.iv};
+            SimpleAdapter simpleAdapter = new SimpleAdapter(getContext(), itemList, R.layout.coupon_layout, from, to);
+            listView.setAdapter(simpleAdapter);
+        }
+        return view;
     }
 }
